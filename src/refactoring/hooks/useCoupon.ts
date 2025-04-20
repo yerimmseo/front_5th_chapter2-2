3d5ab821a8ad5d29b8c4b44b49f98b1b19fe07d7
yerimmseo <yerim7386@naver.com>
2025-04-21 00:15:59 +0900

    useCoupon 훅 생성

--- src/refactoring/hooks/useCoupon.ts
+++ src/refactoring/hooks/useCoupon.ts
@@ -2,5 +2,13 @@ import { useState } from "react";
 import { Coupon } from "../../types.ts";
 
 export const useCoupons = (initialCoupons: Coupon[]) => {
-  return { coupons: [...initialCoupons], addCoupon: () => undefined };
+  const [coupons, setCoupons] = useState<Coupon[]>(initialCoupons);
+
+  const addCoupon = (newCoupon: Coupon) => {
+    // ...coupons, newCoupon <= 직접 상태를 참조하면 잠재적 문제가 발생할 수 있음
+    // 클로저에 의해 캡처된 coupons 값을 사용하므로, 여러 상태 업데이트가 짧은 시간에 연속으로 발생할 경우 최신 상태를 참조하지 못할 수 있다.
+    setCoupons((currentCoupons) => [...currentCoupons, newCoupon]);
+  };
+
+  return { coupons, addCoupon };
 };
