1fc4aa131e1ab39d10483ae85a4a50419f824c13
yerimmseo <yerim7386@naver.com>
2025-04-21 00:27:21 +0900

    useProduct 훅 생성

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -2,9 +2,30 @@ import { useState } from "react";
 import { Product } from "../../types.ts";
 
 export const useProducts = (initialProducts: Product[]) => {
+  const [products, setProducts] = useState<Product[]>(initialProducts);
+
+  const updateProduct = (newProduct: Product) => {
+    setProducts((currentProducts) => {
+      const existingProduct = currentProducts.findIndex(
+        (product) => product.id === newProduct.id
+      );
+
+      if (existingProduct > -1) {
+        const updateProduct = [...currentProducts];
+        updateProduct[existingProduct] = newProduct;
+        return updateProduct;
+      }
+      return currentProducts;
+    });
+  };
+
+  const addProduct = (product: Product) => {
+    setProducts((currentProducts) => [...currentProducts, product]);
+  };
+
   return {
-    products: [...initialProducts],
-    updateProduct: () => undefined,
-    addProduct: () => undefined,
+    products,
+    updateProduct,
+    addProduct,
   };
 };
