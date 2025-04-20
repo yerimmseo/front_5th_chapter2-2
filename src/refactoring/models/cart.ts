435e3f79f3fc99ee3700f7cc307a4fffcf885809
yerimmseo <yerim7386@naver.com>
2025-04-21 01:27:57 +0900

    cartUtils 작성 - calculateCartTotal - updateCartItemQuantity

--- src/refactoring/models/cart.ts
+++ src/refactoring/models/cart.ts
@@ -12,10 +12,49 @@ export const calculateCartTotal = (
   cart: CartItem[],
   selectedCoupon: Coupon | null
 ) => {
+  // 각 아이템 수량 별 할인 적용
+  const pricedCartItems = cart.map((item) => {
+    const totalPrice = item.product.price * item.quantity;
+
+    const applicableDiscount = item.product.discounts
+      .filter((rule) => item.quantity >= rule.quantity)
+      .sort(
+        (a, b) => b.quantity - a.quantity // 가장 높은 수량의 할인율 적용
+      )[0];
+
+    const discountRate = applicableDiscount?.rate ?? 0;
+    const discountAmount = totalPrice * discountRate;
+
+    return { totalPrice, discountAmount };
+  });
+
+  const totalBeforeDiscount = pricedCartItems.reduce(
+    (sum, item) => sum + item.totalPrice,
+    0
+  );
+
+  const productDiscount = pricedCartItems.reduce(
+    (sum, item) => sum + item.discountAmount,
+    0
+  );
+
+  const afterProductDiscounts = totalBeforeDiscount - productDiscount;
+
+  // 쿠폰 할인 계산
+  const couponDiscount = selectedCoupon
+    ? selectedCoupon.discountType === "amount"
+      ? Math.min(selectedCoupon.discountValue, afterProductDiscounts)
+      : (afterProductDiscounts * selectedCoupon.discountValue) / 100
+    : 0;
+
+  const totalDiscount = productDiscount + couponDiscount;
+
+  const totalAfterDiscount = totalBeforeDiscount - totalDiscount;
+
   return {
-    totalBeforeDiscount: 0,
-    totalAfterDiscount: 0,
-    totalDiscount: 0,
+    totalBeforeDiscount,
+    totalDiscount,
+    totalAfterDiscount,
   };
 };
 
@@ -24,5 +63,29 @@ export const updateCartItemQuantity = (
   productId: string,
   newQuantity: number
 ): CartItem[] => {
-  return [];
+  const existingCartItemIndex = cart.findIndex(
+    (item) => item.product.id === productId
+  );
+
+  if (existingCartItemIndex === -1) {
+    return cart;
+  }
+
+  if (newQuantity <= 0) {
+    return cart.filter((item) => item.product.id !== productId);
+  }
+
+  const productStock = cart[existingCartItemIndex].product.stock;
+
+  if (newQuantity > productStock) {
+    newQuantity = productStock;
+  }
+
+  const updatedCart = [...cart];
+  updatedCart[existingCartItemIndex] = {
+    ...updatedCart[existingCartItemIndex],
+    quantity: newQuantity,
+  };
+
+  return updatedCart;
 };
